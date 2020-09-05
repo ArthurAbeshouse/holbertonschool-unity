102d73e9bb56bbb23840cc6460f9550f91b32ffe
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-09-04 22:01:16 -0400

    initial push

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Transform player;
+
+    private Vector3 offset;
+
+    public float turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offset;
+        transform.position = player.position + offset;
+        transform.LookAt(player.position);
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity;
+    public bool isGrounded = true;
+
+    [Range(1, 10)]
+    public float jumpSpeed;
+
+    Rigidbody rbdy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbdy = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocity * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, velocity * Input.GetAxis("Vertical") * Time.deltaTime);
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rbdy.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+}
