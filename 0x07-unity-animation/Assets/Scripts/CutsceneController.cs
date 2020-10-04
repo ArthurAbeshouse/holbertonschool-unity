e2f66a81fff8dc7ea00efaeb392f4821233782ef
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-10-04 16:02:45 -0400

    replaced placeholder pill with character

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public PlayerController Player;
+    public GameObject MainCamera;
+    public GameObject Timer;
+
+    public float delayTime;
+   // private PlayerScript PS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PS = Player.GetComponent<PlayerController>();
+        StartCoroutine(Begin(delayTime));
+    }
+
+    IEnumerator Begin(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        MainCamera.SetActive(true);
+        Timer.SetActive(true);
+        Player.enabled = true;
+        transform.gameObject.SetActive(false);
+    }
+}
