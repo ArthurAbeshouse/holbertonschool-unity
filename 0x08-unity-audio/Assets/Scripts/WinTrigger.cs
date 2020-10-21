9c39e77ce74d0c47ba7f698bff21341662c4fdc0
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2020-10-21 16:10:25 -0400

    BGM stops when the player touches the flagpole

--- 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
+++ 0x08-unity-audio/Assets/Scripts/WinTrigger.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class WinTrigger : MonoBehaviour
 {
     public GameObject Player;
+    public AudioSource CamAudio;
 
     private void OnTriggerEnter(Collider collision)
     {
         Player.GetComponent<Timer>().enabled = false;
+        CamAudio.Stop();
     }
 }
